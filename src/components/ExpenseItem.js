5163d7ed17e440b273cc26d7bb98b4aaa9140dd0
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-08-03 16:26:48 +0600

    added the css styling file and changed the contents of the component.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,14 @@
 function ExpenseItem() {
-    return <h2>Expense Item!</h2>
+    return (
+        <>
+            <h2>Morning Breakfast at DU Campus</h2>
+            <div>
+                <div>21st March, 2021</div>
+                <div>50 BDT</div>
+                <div>Had a moderate breakfast in a cafe in DU.</div>
+            </div>
+        </>
+    );
 }
 
 export default ExpenseItem;
