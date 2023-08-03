96c8820122d7b5a013d1ee17e54c0fbf5cfc7075
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-08-03 17:18:17 +0600

    placed variable values in the text content of the components.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
+import './ExpenseItem.css';
+
 function ExpenseItem() {
+    const expenseDate = new Date(2021, 3, 21);
+    const expenseTitle = 'Bus Fare';
+    const expenseAmount = 30;
+    const expenseDescription = `Journey on bus from Mirpur To Gulshan.`;
     return (
-        <>
-            <h2>Morning Breakfast at DU Campus</h2>
-            <div>
-                <div>21st March, 2021</div>
-                <div>50 BDT</div>
-                <div>Had a moderate breakfast in a cafe in DU.</div>
+        <div className="expense-item">
+            <h2>{expenseTitle}</h2>
+            <div className="expense-item__description">
+                <div>{expenseDate.toDateString()}</div>
+                <div className="expense-item__price">{expenseAmount} BDT</div>
+                <div>{expenseDescription}</div>
             </div>
-        </>
+        </div>
     );
 }
 
