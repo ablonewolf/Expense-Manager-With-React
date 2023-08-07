abd29852514f70cbe71eaf5b61c3c48bb3390728
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-08-08 05:24:57 +0600

    Formatted the date to month, day and year individually and added css styling for description to make the page more readable.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,20 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    const month = props.expense.date.toLocaleString('en-US', {month: 'long'});
+    const day = props.expense.date.toLocaleString('en-US', {day: '2-digit'});
+    const year = props.expense.date.getFullYear();
     return (
         <div className="expense-item">
-            <h2>{props.expense.title}</h2>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__description">
-                <div>{props.expense.date.toDateString()}</div>
+                <h2>{props.expense.title}</h2>
                 <div className="expense-item__price">{props.expense.amount} BDT</div>
-                <div>{props.expense.amount}</div>
+                <h3>{props.expense.description}</h3>
             </div>
         </div>
     );
