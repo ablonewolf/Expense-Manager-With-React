d3c68ebfbe66ca29ed006564b76c1318fa42d6a1
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-08-07 06:52:04 +0600

    used props to pass item dynamically in ExpenseItem.js.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+    const expenses = [
+        {
+            id: 1,
+            title: "Lunch at DU Cafe",
+            date: new Date(2021, 2, 18),
+            amount: 100,
+            description: "Outdoor lunch as I was in DU Campus."
+        },
+        {
+            id: 2,
+            title: "Phone Back-cover",
+            date: new Date(2021, 5, 24),
+            amount: 300,
+            description: "New Back cover for newly bought smartphone. "
+        },
+        {
+            id: 3,
+            title: "Bus Fare",
+            date: new Date(2021, 3, 11),
+            amount: 30,
+            description: "Bus Journey from Mirpur to Gulshan."
+        }
+    ]
+
     return (
         <div>
             <h2>Let's get started!</h2>
-            <ExpenseItem/>
+            <ExpenseItem
+                title={expenses[0].title}
+                date={expenses[0].date}
+                amount={expenses[0].amount}
+                description={expenses[0].description}
+            />
+            <ExpenseItem
+                title={expenses[1].title}
+                date={expenses[1].date}
+                amount={expenses[1].amount}
+                description={expenses[1].description}
+            />
+            <ExpenseItem
+                title={expenses[2].title}
+                date={expenses[2].date}
+                amount={expenses[2].amount}
+                description={expenses[2].description}
+            />
         </div>
     );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 3, 21);
-    const expenseTitle = 'Bus Fare';
-    const expenseAmount = 30;
-    const expenseDescription = `Journey on bus from Mirpur To Gulshan.`;
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <h2>{expenseTitle}</h2>
+            <h2>{props.title}</h2>
             <div className="expense-item__description">
-                <div>{expenseDate.toDateString()}</div>
-                <div className="expense-item__price">{expenseAmount} BDT</div>
-                <div>{expenseDescription}</div>
+                <div>{props.date.toDateString()}</div>
+                <div className="expense-item__price">{props.amount} BDT</div>
+                <div>{props.amount}</div>
             </div>
         </div>
     );
