8cc661ab2bb5d23c0eba18930a534daa78ba5db0
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-08-08 10:45:23 +0600

    added a h1 tag to label the item list, with appropriate styling.

--- src/components/DisplayExpenses.js
+++ src/components/DisplayExpenses.js
@@ -6,6 +6,7 @@ export function DisplayExpenses(props) {
     return (
         // eslint-disable-next-line array-callback-return
         <div className='expenses'>
+            <h1>Expense Item List</h1>
             {props.expenses.map(expenseItem => {
                 return <ExpenseItem
                     expense={expenseItem}
