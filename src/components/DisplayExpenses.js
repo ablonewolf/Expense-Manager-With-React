d23a7ce4579bc59ccf274f438b295f5dbcde6255
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2023-08-08 10:59:35 +0600

    used a wrapper component to include common styles, thus avoid code duplication.

--- src/components/DisplayExpenses.js
+++ src/components/DisplayExpenses.js
@@ -1,17 +1,18 @@
 import ExpenseItem from "./ExpenseItem";
 import React from "react";
 import './Expenses.css';
+import {Card} from "./Card";
 
 export function DisplayExpenses(props) {
     return (
         // eslint-disable-next-line array-callback-return
-        <div className='expenses'>
+        <Card className='expenses'>
             <h1>Expense Item List</h1>
             {props.expenses.map(expenseItem => {
                 return <ExpenseItem
                     expense={expenseItem}
                 />
             })}
-        </div>
+        </Card>
     )
 }
